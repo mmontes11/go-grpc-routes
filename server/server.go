119e9f7528deda62aa0e458a4eae71a9a8a67976
Martín Montes <martin11lrx@gmail.com>
2020-06-26 15:08:34 +0200

    Added GetFeature rpc method

--- server/server.go
+++ server/server.go
@@ -2,16 +2,31 @@ package server
 
 import (
 	ctx "context"
+	"errors"
+	"sync"
 
+	"github.com/golang/protobuf/proto"
 	pb "github.com/mmontes11/go-grpc-routes/route"
 	"google.golang.org/grpc"
 )
 
 type routeServer struct {
+	pb.UnimplementedRouteServer
+	savedFeatures []*pb.Feature
+
+	mux        sync.Mutex
+	routeNotes map[string][]*pb.RouteNote
 }
 
+var errNotFound = errors.New("Not found")
+
 func (s *routeServer) GetFeature(ctx ctx.Context, point *pb.Point) (*pb.Feature, error) {
-	return nil, nil
+	for _, feature := range s.savedFeatures {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
+	return nil, errNotFound
 }
 
 func (s *routeServer) ListFeatures(rect *pb.Rectangle, stream pb.Route_ListFeaturesServer) error {
